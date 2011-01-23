c2f4c6c305974ec2520a8398121c0c3181a08713
Akhyar <akhyrul@gmail.com>
2011-01-23 18:53:44 +0700

    benerin link login di footer halaman utama

--- application/views/tpl_footer.php
+++ application/views/tpl_footer.php
@@ -1,7 +1,11 @@
 		<?php $this->load->view('form_search') ?>
 		<div id="footer">
 			&copy; 2011 Jurusan Teknik Informatika Universitas Islam Indonesia <br />
-			<?php echo anchor('login', '[Login to this site]')?>
+			<?php 
+			if ( ! $this->Otoritas->hasRights() )
+				echo anchor('login', '[Login to this site]');
+			
+			?>
 		</div>
 	</div>
 </body>
